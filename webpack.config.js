d7aebd69b6c9a6f9a643e8ac6fa025cc83983d89
robbierotman <robbierotman@gmail.com>
2025-07-02 14:41:12 +1000

    fix - upgraded packages and fixed minor issues

--- webpack.config.js
+++ webpack.config.js
@@ -1,39 +1,35 @@
-const webpack = require('webpack');
-const path = require('path');
-const UnminifiedWebpackPlugin = require('unminified-webpack-plugin');
-const CleanWebpackPlugin = require('clean-webpack-plugin');
+const webpack = require("webpack");
+const path = require("path");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = {
-    entry: {
-        htmldiff: ['babel-polyfill', './src/Diff.js'],
-    },
+  entry: {
+    htmldiff: ["babel-polyfill", "./src/Diff.js"],
+  },
 
-    output: {
-        filename: 'htmldiff.min.js',
-        path: path.resolve(__dirname, 'dist'),
-        publicPath: '/dist/',
-        library: 'HtmlDiff',
-        libraryTarget: 'commonjs2'
-    },
+  output: {
+    filename: "htmldiff.min.js",
+    path: path.resolve(__dirname, "dist"),
+    publicPath: "/dist/",
+    library: "HtmlDiff",
+    libraryTarget: "commonjs2",
+  },
 
-    module: {
-        rules: [
-            {
-                test: /\.m?js$/,
-                exclude: /(node_modules|bower_components)/,
-                use: {
-                    loader: 'babel-loader',
-                }
-            }
-        ]
-    },
-
-    plugins: [
-        new CleanWebpackPlugin(['dist']),
-        new UnminifiedWebpackPlugin()
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
     ],
+  },
 
-    optimization: {
-        minimize: true
-    }
+  plugins: [new CleanWebpackPlugin()],
+
+  optimization: {
+    minimize: true,
+  },
 };
